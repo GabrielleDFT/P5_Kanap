23cdfa6bb2c05e01c8d3a35825ac557d21f12082
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-04-11 20:01:40 +0200

    Update "index.js"

--- front/js/index.js
+++ front/js/index.js
@@ -1,43 +1,50 @@
 
-// CALL TO API
-displayProducts();
-async function getProducts() {// GET method to retrieve Data from API
+// ---------------------A Call to API (to Display Items)----------------------------------
+//displayProducts();
+async function getProducts() {//GET method to retrieve Data from API
     return await fetch("http://localhost:3000/api/products")
   .then(function(res) {
     if (res.ok) {
-      return res.json(); //Retrieving objects from server + Convert to Json
+      return res.json(); //Return objects from server + Convert to Json
     }
   })
-  .then(function(value) {//return result + retrieve color
+  .then(function(value) {//Return result + ?retrieve color?
     console.log(value);
     return value;
   })
-  .catch(function(err) {// in case of error
+  .catch(function(err) {//Case of error
     });
 }
 
-// DATA INTEGRATION IN DOM : Retrieving + Display Products 
+//-------------------Data Insert to the Dom : Retrieve + Display Items-------------------------
 async function displayProducts() {
-  const parser = new DOMParser();
-  const products = await getProducts();
- console.log("displayProducts", products);
- let productsSection = document.getElementById("items"); 
-   
- // Loop to Iterate Products
- for (let i = 0; i < products.length; i++) { 
-  //intégration du HTML
-  let productsItems = `
- <a href="./product.html?id=${products[i]._id}"> 
- <article>
-   <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
-   <h3 class="productName">${products[i].name}</h3>
-   <p class="productDescription">${products[i].description}</p>
- </article>
-</a>`;
-const displayShop = parser.parseFromString(productsItems, "text/html");
-productsSection.appendChild(displayShop.body.firstChild);
+    const parser = new DOMParser();
+    const items = await getProducts();
+    console.log("displayProducts", items);
+    var products = document.getElementById("items"); 
+    
+    for (let i = 0; i < items.length; i++) { // Loop to Iterate Products
+
+        var ItemsDetails =   // HTML insert
+        `<a href="./product.html?id=${items[i]._id}"> 
+        <article>
+        <img src="${items[i].imageUrl}" alt="${items[i].altTxt}">
+        <h3 class="productName">${items[i].name}</h3>
+        <p class="productDescription">${items[i].description}</p>
+        </article>
+        </a>`;
+        const displayShop = parser.parseFromString(ItemsDetails, "text/html");
+        products.appendChild(displayShop.body.firstChild);
+    }
 }
-}
+
+
+ 
+
+  
+
+
+
 
 
  
