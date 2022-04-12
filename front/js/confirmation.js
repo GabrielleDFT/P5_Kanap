4e251cf2c92564933095feb73bbd86dfcc6fe059
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-04-12 23:49:18 +0200

    Update confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,11 +1,11 @@
 
 // ---------------------------------CART CONFIRMATION NUMBER-------------------------------------------
-function confirmation() {
-    const ConfirmNumber = document.getElementById("orderId");
-    ConfirmNumber.innerText = localStorage.getItem("orderId");
-    console.log("Id de la commande");
-    console.log(localStorage.getItem("orderId"));
-    localStorage.clear();
-  }
-  confirmation();
+//function confirmation() {
+//    const ConfirmNumber = document.getElementById("orderId");
+//    ConfirmNumber.innerText = localStorage.getItem("orderId");
+//    console.log("Id de la commande");
+//    console.log(localStorage.getItem("orderId"));
+//    localStorage.clear();
+//  }
+//  confirmation();
   
