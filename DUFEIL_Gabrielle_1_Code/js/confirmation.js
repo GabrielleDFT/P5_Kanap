9c9efa439898278c547797c333641b90800d1de5
GabrielleDFT <96197835+GabrielleDFT@users.noreply.github.com>
2022-04-25 21:06:09 +0200

    Delete confirmation.js

--- DUFEIL_Gabrielle_1_Code/js/confirmation.js
+++
@@ -1,8 +0,0 @@
-
-//---------------------------------CART CONFIRMATION NUMBER-------------------------------------------
-        //--Récupération Order Id--
-const params = new URL(document.location).searchParams;
-const orderId = params.get("orderId");
-
-      //--Display du Order Number--
-document.getElementById("orderId").textContent = orderId;
